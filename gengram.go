27fe3a595ebfc69765b7144c904de59354723e9d
alkaaf <alfa.alkaaf@gmail.com>
2023-07-21 16:24:10 +0700

    fix package

--- gengram.go
+++ gengram.go
@@ -16,13 +16,17 @@ func Ngram(phrases []string, min int, max int) []string {
 		words := strings.Split(phrase, " ")
 		for _, word := range words {
 			if len(word) < min {
-				ngrams = append(ngrams, word)
+				if !checkStringInSlice(word, ngrams) {
+					ngrams = append(ngrams, word)
+				}
 				continue
 			}
 			for i := min; i <= max; i++ {
 				for j := 0; j < len(word)-i+1; j++ {
 					cut := word[j : j+i]
-					ngrams = append(ngrams, cut)
+					if !checkStringInSlice(cut, ngrams) {
+						ngrams = append(ngrams, cut)
+					}
 				}
 			}
 		}
@@ -30,7 +34,17 @@ func Ngram(phrases []string, min int, max int) []string {
 	return ngrams
 }
 
+// create a function to check specific string in slice
+func checkStringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	x := []string{"hello world", "i am finding ngram", "this is a test"}
+	x := []string{"hello world", "world"}
 	fmt.Println(Ngram(x, 2, 9))
 }
